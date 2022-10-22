6b9aedab532e8e890e908acc0a1430ed7ae0a444
Студент1 <1@DESKTOP-7RO4MU5>
2022-10-22 11:45:27 +0300

    Добавьте файлы проекта.

---
+++ glasn/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите строку");
+string select = Console.ReadLine();
+char[] a = select.ToCharArray();
+char[] b = new char[] { 'a', 'e', 'i', 'o', 'u' };
+    foreach (char glasn in b)
+    {
+        bool isupper = char.IsUpper(glasn);
+        if (isupper == false)
+        {
+            select = select.Replace(glasn.ToString(), "");
+        }
+    }
+Console.WriteLine();
+Console.WriteLine(select);
